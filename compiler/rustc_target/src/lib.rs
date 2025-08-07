2169980e0afd8b3f66bef56a57675b69e2a91bd7
Jan Alexander Steffens (heftig) <heftig@archlinux.org>
2025-08-07 20:12:53 +0200

    ARCHLINUX: compiler: Swap primary and secondary lib dirs
    Arch Linux (editor's note: also AOSC OS) prefers "lib" over "lib64".

--- compiler/rustc_target/src/lib.rs
+++ compiler/rustc_target/src/lib.rs
@@ -53,20 +53,22 @@ fn find_relative_libdir(sysroot: &Path) -> std::borrow::Cow<'static, str> {
     // If --libdir is set during configuration to the value other than
     // "lib" (i.e., non-default), this value is used (see issue #16552).
 
+    const PRIMARY_LIB_DIR: &str = "lib";
+
     #[cfg(target_pointer_width = "64")]
-    const PRIMARY_LIB_DIR: &str = "lib64";
+    const SECONDARY_LIB_DIR: &str = "lib64";
 
     #[cfg(target_pointer_width = "32")]
-    const PRIMARY_LIB_DIR: &str = "lib32";
-
-    const SECONDARY_LIB_DIR: &str = "lib";
+    const SECONDARY_LIB_DIR: &str = "lib32";
 
     match option_env!("CFG_LIBDIR_RELATIVE") {
         None | Some("lib") => {
             if sysroot.join(PRIMARY_LIB_DIR).join(RUST_LIB_DIR).exists() {
                 PRIMARY_LIB_DIR.into()
-            } else {
+            } else if sysroot.join(SECONDARY_LIB_DIR).join(RUST_LIB_DIR).exists() {
                 SECONDARY_LIB_DIR.into()
+            } else {
+                PRIMARY_LIB_DIR.into()
             }
         }
         Some(libdir) => libdir.into(),
